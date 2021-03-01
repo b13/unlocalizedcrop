8e3a4313338eb341d6dbf34d3f190f7caca12bfb
Benni Mack <benni@typo3.org>
2021-03-01 07:35:16 +0100

    [BUGFIX] Fix hook registration to reflect new class name

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,4 +2,4 @@
 defined('TYPO3_MODE') or die();
 
 // Register a DataHandler hook to always set the crop value for all children when updating a parent
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['unlocalizedcrop'] = \CMSExperts\Unlocalizedcrop\Hooks\DataHandlerCropModifier::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['unlocalizedcrop'] = \B13\Unlocalizedcrop\Hooks\DataHandlerCropModifier::class;
