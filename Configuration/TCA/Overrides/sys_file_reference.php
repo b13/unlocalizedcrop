874eefb637c93395f27b1bd7c9147bc66108114f
Benni Mack <benni@typo3.org>
2023-02-23 13:09:37 +0100

    [TASK] Streamline composer file + setup

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -1,5 +1,6 @@
 <?php
-defined('TYPO3_MODE') or die();
+
+defined('TYPO3_MODE') or defined('TYPO3') or die();
 
 // only show the cropping field for records that have no localization parent
 $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['l10n_mode'] = 'exclude';
